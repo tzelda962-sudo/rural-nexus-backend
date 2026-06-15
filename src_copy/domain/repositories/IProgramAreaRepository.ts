fd53bc056b31ef6cfa168015b0790604be9acaa9
Kum Brian Ihimbru <kumbrianihimbru@gmail.com>
2026-06-15 12:31:26 +0100

    refactor: remove entire src_copy directory and associated legacy task documentation files

--- src_copy/domain/repositories/IProgramAreaRepository.ts
+++
@@ -1,6 +0,0 @@
-import type { ProgramArea } from "../entities/ProgramArea";
-
-export interface IProgramAreaRepository {
-  getAll(): Promise<ProgramArea[]>;
-  getById(id: string): Promise<ProgramArea | null>;
-}
